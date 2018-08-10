019c98de6438dc7c6b391d42de5c40fae36d4503
kgkaudi <konstantinos.gkaris@gmail.com>
2018-08-10 14:52:40 +0200

    Shots
    Shots now can be fired

---
+++ Ikaruga/Assets/Scripts/BulletKiller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletKiller : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+		Destroy (other.gameObject);
+	}
+}
---
+++ Ikaruga/Assets/Scripts/Shot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+	public float speed;
+	public float damage;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+	}
+
+}
